536825dc62244e029c9475fcf5e7dd4451375987
juanrodriguezmt <91926824+juanrodriguezmt@users.noreply.github.com>
2021-10-30 01:31:01 -0500

    Update ControladorCategory.java

--- src/main/java/CicloReto3/CicloReto3/ControladorCategory.java
+++ src/main/java/CicloReto3/CicloReto3/ControladorCategory.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Category")
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 
 public class ControladorCategory {
     @Autowired
