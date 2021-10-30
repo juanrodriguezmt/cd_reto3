2c7c54547543744cb59298ae35fff1dff18adc80
juanrodriguezmt <91926824+juanrodriguezmt@users.noreply.github.com>
2021-10-29 20:21:48 -0500

    Update Client.java

--- src/main/java/CicloReto3/CicloReto3/Client.java
+++ src/main/java/CicloReto3/CicloReto3/Client.java
@@ -18,9 +18,9 @@ public class Client implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     
     private Integer idClient;
-    private String name;
     private String email;
     private String password;
+    private String name;
     private Integer age;
     
     
