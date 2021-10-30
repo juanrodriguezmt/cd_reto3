56655f252cb9ac0b7922c1f9e77ffa8c8d377b31
juanrodriguezmt <91926824+juanrodriguezmt@users.noreply.github.com>
2021-10-30 00:42:31 -0500

    Update serviciosCategory.java

--- src/main/java/CicloReto3/CicloReto3/serviciosCategory.java
+++ src/main/java/CicloReto3/CicloReto3/serviciosCategory.java
@@ -30,4 +30,32 @@ public class serviciosCategory {
             }
         }
     }
+    
+    
+    public Category update(Category category){
+        if(category.getId()!=null){
+            Optional<Category> e=metodosCrud.getCategory(category.getId());
+            if(!e.isEmpty()){
+                if(category.getDescription()!=null){
+                    e.get().setDescription(category.getDescription());
+                }
+                if(category.getName()!=null){
+                    e.get().setName(category.getName());
+                }
+                metodosCrud.save(e.get());
+                return e.get();
+            }else{
+                return category;}
+        }else{
+            return category;}
+    
+    }
+    
+    public boolean deleteCategory(int categoryId){
+        Boolean aBoolean = getCategory(categoryId).map(category -> {
+            metodosCrud.delete(category);
+            return true;
+        }).orElse(false);
+        return aBoolean;
+        }
 }
