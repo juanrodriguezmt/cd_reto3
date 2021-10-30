83b976f16bd6ad7e6f4a9c0949294767480feff4
juanrodriguezmt <91926824+juanrodriguezmt@users.noreply.github.com>
2021-10-30 01:09:06 -0500

    Update ControladorMessage.java

--- src/main/java/CicloReto3/CicloReto3/ControladorMessage.java
+++ src/main/java/CicloReto3/CicloReto3/ControladorMessage.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,15 @@ public class ControladorMessage {
     public Message save(@RequestBody Message message){
         return servicios.save(message);
     }
+    @PostMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Message update(@RequestBody Message message){
+        return servicios.update(message);
+    }
     
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int messageId){
+        return servicios.deleteMessage(messageId);
+    }
 }
